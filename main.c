de5e04035dc14181ffa088b31b215e94ba4968bc
MATEUS DO PRADO ALCÂNTARA <mateprado123@gmail.com>
2025-11-26 07:43:23 -0300

    Retirando a parte de consulta, corrigindo Bug's  na leitura e a limpeza de memórias usadas.
    Fiz alteração estrutural do swicth principal, tirando a parte de consulta, para disponibilizar linhas, das quais foram designadas para fazer as correções de leituras.
    
    Está versão contém ERRO.
    
    TESTAR: Todas as entradas/ leituras de dados, para ver se vai ocorrer os loops infinitos.
    Co-Authored-By: Mateus Momberg <245076273+mateusmomberg-bot@users.noreply.github.com>

--- main.c
+++ main.c
@@ -4,69 +4,64 @@ int main(){
 	int codigos[] = {101, 102, 103, 104, 105};
 	char nomes[][20] = {"Arroz", "Feijao", "Macarrao", "Farinha", "Acucar"};
 	float precos[] = {5.5, 7.2, 4.0, 3.8, 2.5};
-	int carrinho[10][3];
+	double carrinho[10][3];
 	
 	int entrada;
-	int n_consulta;
+	int leitura_entrada;
 	int n_adiciona;
 	int linha_carrinho = 0;
-	int cod_produto;
 	int qtd_produto;
 	int i;
 	double subtotal;
+	double total_geral = 0.0;
 	
 	do {
 		
 		printf("|--------------------------------------------------|\n");
 		printf("| Codigos: 101, 102, 103, 104, 105                 |\n");
 		printf("| Nomes: Arroz, Feijao, Macarrao, Farinha, Acucar  |\n");
-		printf("| Precos: 5.5, 7.2, 4.0, 3.8, 2.5                  |\n");
+		printf("| Precos:  R$ 5.5, 7.2, 4.0, 3.8, 2.5              |\n");
 		printf("|--------------------------------------------------|\n");
 		printf("| Escolha uma opcao abaixo:                        |\n");
-		printf("| 1 - Consultar um produto                         |\n");
-		printf("| 2 - Adicionar um produto ao carrinho             |\n");
-		printf("| 3 - Sair                                         |\n");
+		printf("| 1 - Adicionar um produto ao carrinho             |\n");
+		printf("| 2 - Sair                                         |\n");
 		printf("|--------------------------------------------------|\n");
 			
-		scanf("%d", &entrada);
+		leitura_entrada = scanf("%d", &entrada);
+		//Verificando a leitura de outros caracteres
+		if ( leitura_entrada != 1) { //Limpeza de mémoria usada 
+					while (getchar() != '\n');
+					entrada = 0;
+					printf("Entrada ou Opcao invalida. INSIRA OS VALORES DA TABELA PRE-FIXADA!!! \n");
+					continue;
+		}
 		switch(entrada){
-		case 1:
-			printf("Insira o codigo do produto que deseja consultar:\n");
-			scanf("%d", &n_consulta);
-			switch(n_consulta){
-				case 101:
-					printf("Produto: Arroz \n");
-					printf("Preco: R$ 5.50 \n");
-					break;
-				case 102:
-					printf("Produto: Feijao \n");
-					printf("Preco: R$ 7.20 \n");
-					break;
-				case 103:
-					printf("Produto: Macarrao \n");
-					printf("Preco: R$ 4.00 \n");
-					break;
-				case 104:
-					printf("Produto: Farinha \n");
-					printf("Preco: R$ 3.80 \n");
-					break;
-				case 105:
-					printf("Produto: Acucar \n");
-					printf("Preco: R$ 2.50 \n");
-					break;
-				default:
-					printf("Codigo errado. \n");
-					break;
+		
+		case 1: 
+			if(linha_carrinho>=10){
+			printf("O carrinho esta cheio! Maximo de 10 itens atingido.\n");	
+			break;
 			}
 			
-		case 2: 
-		
 			printf("Digite o codigo do produto para adicionar:\n");
-			scanf("%d", &n_adiciona);
+			leitura_entrada = scanf("%d", &n_adiciona);
+			
+			if (leitura_entrada != 1) { //Limpeza de mémoria usada 
+					while (getchar() != '\n');
+					printf("Codigo Invalido.\n");
+					break;
+			}
+		
 			switch(n_adiciona){
 				case 101:
 					printf("Digite o quantidade desejada:\n");
-					scanf("%d", &qtd_produto);
+					leitura_entrada = scanf("%d", &qtd_produto);
+					
+					if (leitura_entrada != 1) { 
+					while (getchar() != '\n');
+					printf("Quantidade Invalida.\n");
+					break;
+					}
 					if (qtd_produto <= 0){
 					printf("Quantidade Insuficiente. \n");	
 					break;
@@ -78,10 +73,9 @@ int main(){
 						}
 					subtotal = 5.50 * (double)qtd_produto;
 					
-					
-					carrinho[linha_carrinho][0] = n_adiciona;        
-					carrinho[linha_carrinho][1] = qtd_produto;        
-					carrinho[linha_carrinho][2] = (int)subtotal;
+					carrinho[linha_carrinho][0] = (double)n_adiciona;        
+					carrinho[linha_carrinho][1] = (double)qtd_produto;        
+					carrinho[linha_carrinho][2] = subtotal;
 							
 					linha_carrinho++;
 					break;
@@ -91,7 +85,13 @@ int main(){
 				case 102:
 					
 					printf("Digite o quantidade desejada:\n");
-					scanf("%d", &qtd_produto);
+					leitura_entrada = scanf("%d", &qtd_produto);
+					
+					if (leitura_entrada != 1) { 
+					while (getchar() != '\n');
+					printf("Quantidade Invalida.\n");
+					break;
+					}
 					if (qtd_produto <= 0){
 					printf("Quantidade Insuficiente. \n");	
 					break;
@@ -103,10 +103,9 @@ int main(){
 						}
 					subtotal = 7.20 * (double)qtd_produto;
 					
-					
-					carrinho[linha_carrinho][0] = n_adiciona;        
-					carrinho[linha_carrinho][1] = qtd_produto;        
-					carrinho[linha_carrinho][2] = (int)subtotal;
+					carrinho[linha_carrinho][0] = (double)n_adiciona;        
+					carrinho[linha_carrinho][1] = (double)qtd_produto;    
+					carrinho[linha_carrinho][2] = subtotal;
 							
 					linha_carrinho++;
 					break;
@@ -116,7 +115,13 @@ int main(){
 				case 103:
 					
 					printf("Digite o quantidade desejada:\n");
-					scanf("%d", &qtd_produto);
+					leitura_entrada = scanf("%d", &qtd_produto);
+					
+					if (leitura_entrada != 1) { 
+					while (getchar() != '\n');
+					printf("Quantidade Invalida.\n");
+					break;
+					}
 					if (qtd_produto <= 0){
 					printf("Quantidade Insuficiente. \n");	
 					break;
@@ -129,9 +134,9 @@ int main(){
 					subtotal = 4.00 * (double)qtd_produto;
 					
 					
-					carrinho[linha_carrinho][0] = n_adiciona;        
-					carrinho[linha_carrinho][1] = qtd_produto;        
-					carrinho[linha_carrinho][2] = (int)subtotal;
+					carrinho[linha_carrinho][0] = (double)n_adiciona;        
+					carrinho[linha_carrinho][1] = (double)qtd_produto;          
+					carrinho[linha_carrinho][2] = subtotal;
 							
 					linha_carrinho++;
 					break;
@@ -141,7 +146,13 @@ int main(){
 				case 104:
 					
 					printf("Digite o quantidade desejada:\n");
-					scanf("%d", &qtd_produto);
+					leitura_entrada = scanf("%d", &qtd_produto);
+					
+					if (leitura_entrada != 1) { 
+					while (getchar() != '\n');
+					printf("Quantidade Invalida.\n");
+					break;
+					}
 					if (qtd_produto <= 0){
 					printf("Quantidade Insuficiente. \n");	
 					break;
@@ -154,9 +165,9 @@ int main(){
 					subtotal = 3.80 * (double)qtd_produto;
 					
 					
-					carrinho[linha_carrinho][0] = n_adiciona;        
-					carrinho[linha_carrinho][1] = qtd_produto;        
-					carrinho[linha_carrinho][2] = (int)subtotal;
+					carrinho[linha_carrinho][0] = (double)n_adiciona;        
+					carrinho[linha_carrinho][1] = (double)qtd_produto;           
+					carrinho[linha_carrinho][2] = subtotal;
 							
 					linha_carrinho++;
 					break;
@@ -166,41 +177,52 @@ int main(){
 				case 105:
 				
 					printf("Digite o quantidade desejada:\n");
-					scanf("%d", &qtd_produto);
+					leitura_entrada = scanf("%d", &qtd_produto);
+					
+					if (leitura_entrada != 1) { 
+					while (getchar() != '\n');
+					printf("Quantidade Invalida.\n");
+					break;
+					}
 					if (qtd_produto <= 0){
 					printf("Quantidade Insuficiente. \n");	
 					break;
 					}
 					
-					for(i = 0; i < 10; i++){
-						if(codigos[i] == n_adiciona){
-							int indice = i;
-						}
 					subtotal = 2.50 * (double)qtd_produto;
 					
 					
-					carrinho[linha_carrinho][0] = n_adiciona;        
-					carrinho[linha_carrinho][1] = qtd_produto;        
-					carrinho[linha_carrinho][2] = (int)subtotal;
+					carrinho[linha_carrinho][0] = (double)n_adiciona;        
+					carrinho[linha_carrinho][1] = (double)qtd_produto;         
+					carrinho[linha_carrinho][2] = subtotal;
 							
 					linha_carrinho++;
 					break;
-					}
-				break;
-				
+					
 				default:
 					printf("Codigo nao Encontrado. \n");
 					break;
-			}
+					}
 			break;
-		case 3:
+				
+		case 2:
 			printf("Saindo do Sistema e Imprimindo a Nota Fiscal:\n");
 			break;
 		default:
 			printf("Opcao errada! \n");
 			break;
 		} 
+
+	total_geral += subtotal;
 		
-	}while(entrada != 3);
+	}while(entrada != 2);
+	
+	if (entrada == 2 || linha_carrinho ==10) {
+		printf("\n|=============== NOTA FISCAL =========================|\n"); 
+	for (i = 0;i<linha_carrinho; i++){
+		printf("| %s | Cod.: %d | %d UNID| R$ %.2lf -->Subtotal: R$ %.2lf |\n",  nomes[i], (int)carrinho[i][0], (int)carrinho[i][1], carrinho[i][2]/carrinho[i][1], carrinho[i][2]);
+		}	
+		printf("\nTOTAL A PAGAR: R$ %.2lf\n", total_geral);
+	}
 	
 }
